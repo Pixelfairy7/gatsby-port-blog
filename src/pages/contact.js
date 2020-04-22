5229f30270f22abc7c257b1b3cf2f02ba8a2072c
Erika Myles <erikamyles314@gmail.com>
2020-04-21 20:27:53 -0400

    pull from master

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,8 +3,6 @@ import React from "react"
 import { Link } from "gatsby"
 import SEO from "../components/seo"
 
-import ContactStyles from "./contact.module.css"
-
 //  import components
 import Layout from "../components/layout"
 import Heading from "../components/heading/heading"
